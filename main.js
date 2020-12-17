cb6af493859b0d28ed760fb482efa20b1f2aa982
Kreg Hanning <khanning@gmail.com>
2020-12-16 16:18:14 -0800

    Push new build

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 let capture, mainCanvas;
 let startScreen = true;
+let timerInterval = null;
 
 function preload() {
   // logoImg = loadImage('glowdoodle-logo.jpg');
 }
 function setup() {
+
+  const captureButton = document.querySelector('#capture');
+  captureButton.onpointerdown = startCapture;
+  captureButton.onpointerup = stopCapture;
+  captureButton.ontouchend = stopCapture;
+
+  // pixelDensity(displayDensity()/2);
+  // console.log(pixelDensity());
+  pixelDensity(1);
+    // console.log(pixelDensity());
   mainCanvas = createCanvas(windowWidth, windowHeight);
   capture = createCapture({
     video: {
       facingMode: "environment",
-      maxWidth: width,
-      maxHeight: height
+      width: { ideal: width, max: width },
+      height: { ideal: height, max: height },
+      // aspectRatio: { exact: 1.7777777778 }
     },
   });
   capture.hide();
@@ -30,24 +42,57 @@ function draw() {
   } else {
     imageMode(CORNER);
     blendMode(LIGHTEST);
-    scale(-1, 1);
-    translate(-width, 0);
-    image(capture, 0, 0, width, width * capture.height / capture.width);
+    // scale(1, 1);
+    // translate(-width, 0);
+    if (width != capture.width || height != capture.height) {
+        let c = capture.get((capture.width-capture.height)/2, 0, height, height);
+        image(c, 0, 0);
+    } else {
+        image(capture, 0, 0, width, width * capture.height / capture.width);
+    }
   }
 }
 
-function mousePressed(){
-    console.log('mousepressed');
-  if (startScreen) {
+function startTimed() {
+    if (timerInterval) return;
+    const dur = document.querySelector('#duration').value;
+    startCapture();
+    timerInterval = setTimeout(stopCapture, dur*1000);
+}
+
+function clearScreen() {
+    clear();
+}
+
+function startCapture() {
     startScreen = false;
-  }
-  // clear();
-  // background(0);
+
 }
 
-function mouseReleased() {
+function stopCapture() {
     startScreen = true;
+    if (timerInterval) {
+        clearTimeout(timerInterval);
+        timerInterval = null;
+    }
 }
+
+function save() {
+    save('test', 'png');
+}
+
+// function mousePressed(){
+  // console.log('mousepressed');
+  // if (startScreen) {
+    // startScreen = false;
+  // }
+  // // clear();
+  // // background(0);
+// }
+
+// function mouseReleased() {
+    // startScreen = true;
+// }
 
 function keyPressed(){
   if (key === 's') {
